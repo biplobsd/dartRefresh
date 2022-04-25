42a733e4a3435f894acf78869f9c3f932e90703d
Biplob Sutradhar <biplobsd11@gmail.com>
2022-04-25 14:11:52 +0600

    literal

---
+++ types/types.dart
@@ -0,0 +1,10 @@
+void main() {
+  // String literal, also constant value
+  String s = 'hello';
+
+  // int literal, also constant value
+  int p = 3;
+
+  // List literal, also constant value
+  List<int> list = [1, 3, 4];
+}
