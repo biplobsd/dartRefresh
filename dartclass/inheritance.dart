d17da07bb240dcb5f8dfa77bfce34ca91c005ca6
Biplob Sutradhar <biplobsd11@gmail.com>
2022-05-01 16:29:05 +0600

    inheritance

---
+++ dartclass/inheritance.dart
@@ -0,0 +1,12 @@
+class A extends Object {
+  @override
+  String toString() {
+    // TODO: implement toString
+    super.toString();
+    return 'This is a!';
+  }
+}
+
+void main(List<String> args) {
+  print(A().toString());
+}
